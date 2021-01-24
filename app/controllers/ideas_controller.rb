327a23040eadd4f4b2875aa2bbe911c1a1e39bb0
wardhen <widyaazka16@gmail.com>
2021-01-24 12:52:38 +0700

    Add Comments model

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -10,6 +10,8 @@ class IdeasController < ApplicationController
   # GET /ideas/1
   # GET /ideas/1.json
   def show
+    @comments = @idea.comments.all
+    @comments = @idea.comments.build
   end
 
   # GET /ideas/new
